1192838de4fb160c549bacc0ae9fa9dc4664cb1a
JoshuaBPapa <joshua.b.papa@gmail.com>
2023-06-15 17:39:19 +0100

    added base scss

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import PokemonListContainer from './components/PokemonListContainer/PokemonListContainer';
 import PokemonDetailedContainer from './components/PokemonDetailedContainer/PokemonDetailedContainer';
 import { Pokemon } from './interfaces';
+import './style/_index.scss';
 
 const App: React.FC = () => {
   const [selectedPokemon, setSelectedPokemon] = useState<null | Pokemon>(null);
