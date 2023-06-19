73da66a23f35730f5831b43b431ed12fdd8b6e37
JoshuaBPapa <joshua.b.papa@gmail.com>
2023-06-19 16:18:58 +0100

    added svg icons for pokemon types

---
+++ src/imgs/type-backgrounds/index.ts
@@ -0,0 +1,41 @@
+import { ReactComponent as WaterTypeBackground } from './water.svg';
+import { ReactComponent as DragonTypeBackground } from './dragon.svg';
+import { ReactComponent as ElectricTypeBackground } from './electric.svg';
+import { ReactComponent as FairyTypeBackground } from './fairy.svg';
+import { ReactComponent as GhostTypeBackground } from './ghost.svg';
+import { ReactComponent as FireTypeBackground } from './fire.svg';
+import { ReactComponent as IceTypeBackground } from './ice.svg';
+import { ReactComponent as GrassTypeBackground } from './grass.svg';
+import { ReactComponent as BugTypeBackground } from './bug.svg';
+import { ReactComponent as FightingTypeBackground } from './fighting.svg';
+import { ReactComponent as NormalTypeBackground } from './normal.svg';
+import { ReactComponent as DarkTypeBackground } from './dark.svg';
+import { ReactComponent as SteelTypeBackground } from './steel.svg';
+import { ReactComponent as RockTypeBackground } from './rock.svg';
+import { ReactComponent as PsychicTypeBackground } from './psychic.svg';
+import { ReactComponent as GroundTypeBackground } from './ground.svg';
+import { ReactComponent as FlyingTypeBackground } from './flying.svg';
+import { ReactComponent as PoisonTypeBackground } from './poison.svg';
+
+const typeBackgroundMap: { [key: string]: any } = {
+  water: WaterTypeBackground,
+  dragon: DragonTypeBackground,
+  electric: ElectricTypeBackground,
+  fairy: FairyTypeBackground,
+  ghost: GhostTypeBackground,
+  fire: FireTypeBackground,
+  ice: IceTypeBackground,
+  grass: GrassTypeBackground,
+  bug: BugTypeBackground,
+  fighting: FightingTypeBackground,
+  normal: NormalTypeBackground,
+  dark: DarkTypeBackground,
+  steel: SteelTypeBackground,
+  rock: RockTypeBackground,
+  psychic: PsychicTypeBackground,
+  ground: GroundTypeBackground,
+  flying: FlyingTypeBackground,
+  poison: PoisonTypeBackground,
+};
+
+export default typeBackgroundMap;
---
+++ src/imgs/type-icons/index.ts
@@ -0,0 +1,41 @@
+import { ReactComponent as WaterTypeIcon } from './water.svg';
+import { ReactComponent as DragonTypeIcon } from './dragon.svg';
+import { ReactComponent as ElectricTypeIcon } from './electric.svg';
+import { ReactComponent as FairyTypeIcon } from './fairy.svg';
+import { ReactComponent as GhostTypeIcon } from './ghost.svg';
+import { ReactComponent as FireTypeIcon } from './fire.svg';
+import { ReactComponent as IceTypeIcon } from './ice.svg';
+import { ReactComponent as GrassTypeIcon } from './grass.svg';
+import { ReactComponent as BugTypeIcon } from './bug.svg';
+import { ReactComponent as FightingTypeIcon } from './fighting.svg';
+import { ReactComponent as NormalTypeIcon } from './normal.svg';
+import { ReactComponent as DarkTypeIcon } from './dark.svg';
+import { ReactComponent as SteelTypeIcon } from './steel.svg';
+import { ReactComponent as RockTypeIcon } from './rock.svg';
+import { ReactComponent as PsychicTypeIcon } from './psychic.svg';
+import { ReactComponent as GroundTypeIcon } from './ground.svg';
+import { ReactComponent as FlyingTypeIcon } from './flying.svg';
+import { ReactComponent as PoisonTypeIcon } from './poison.svg';
+
+const typeIconMap: { [key: string]: any } = {
+  water: WaterTypeIcon,
+  dragon: DragonTypeIcon,
+  electric: ElectricTypeIcon,
+  fairy: FairyTypeIcon,
+  ghost: GhostTypeIcon,
+  fire: FireTypeIcon,
+  ice: IceTypeIcon,
+  grass: GrassTypeIcon,
+  bug: BugTypeIcon,
+  fighting: FightingTypeIcon,
+  normal: NormalTypeIcon,
+  dark: DarkTypeIcon,
+  steel: SteelTypeIcon,
+  rock: RockTypeIcon,
+  psychic: PsychicTypeIcon,
+  ground: GroundTypeIcon,
+  flying: FlyingTypeIcon,
+  poison: PoisonTypeIcon,
+};
+
+export default typeIconMap;
