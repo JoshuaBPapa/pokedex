776c9786ac5808a9c3b0b4cfc8666de98545c0af
JoshuaBPapa <joshua.b.papa@gmail.com>
2023-06-27 11:37:47 +0100

    styled PokemonAbout

--- src/components/PokemonAbout/PokemonAbout.tsx
+++ src/components/PokemonAbout/PokemonAbout.tsx
@@ -1,4 +1,6 @@
 import { GenderPercentage } from '../../interfaces';
+import { ReactElement } from 'react';
+import './PokemonAbout.scss';
 
 interface Props {
   species: string;
@@ -8,30 +10,38 @@ interface Props {
 }
 
 const PokemonAbout: React.FC<Props> = ({ species, abilities, genderRatio, eggGroups }) => {
-  let gender: string | Element = 'Genderless';
-  if (genderRatio) gender = `♂️${genderRatio.male}%,  ♀${genderRatio.female}%`;
+  let gender: string | ReactElement = 'Genderless';
+  if (genderRatio) {
+    gender = (
+      <div>
+        Male: {genderRatio.male}%, Female: {genderRatio.female}%
+      </div>
+    );
+  }
 
   return (
-    <table>
-      <tbody>
-        <tr>
-          <td>Species</td>
-          <td>{species}</td>
-        </tr>
-        <tr>
-          <td>Abilities</td>
-          <td>{abilities.join(', ')}</td>
-        </tr>
-        <tr>
-          <td>Gender</td>
-          <td>{gender}</td>
-        </tr>
-        <tr>
-          <td>Egg Groups</td>
-          <td>{eggGroups.join(', ')}</td>
-        </tr>
-      </tbody>
-    </table>
+    <div className="pokemon-about-container">
+      <table className="pokemon-about-table">
+        <tbody>
+          <tr>
+            <td>Species</td>
+            <td>{species}</td>
+          </tr>
+          <tr>
+            <td>Abilities</td>
+            <td>{abilities.join(', ')}</td>
+          </tr>
+          <tr>
+            <td>Gender</td>
+            <td>{gender}</td>
+          </tr>
+          <tr>
+            <td>Egg Groups</td>
+            <td>{eggGroups.join(', ')}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 };
 
