eb5a9ea10daaabad23231f84e2120c589a3de64c
JoshuaBPapa <joshua.b.papa@gmail.com>
2023-06-29 14:06:56 +0100

    styled and restructured logic in PokemonDetailedComponent. Implemented useFetchChained to contain all fetch logic in one useEffect and another useEffect to handle building pokemonDetails from successful http responses

--- src/components/PokemonDetailedContainer/PokemonDetailedContainer.tsx
+++ src/components/PokemonDetailedContainer/PokemonDetailedContainer.tsx
@@ -1,13 +1,16 @@
 import { Fragment, ReactNode, useEffect, useState } from 'react';
-import { EvoPath, Pokemon, PokemonDetailed } from '../../interfaces';
-import { useFetch, useWaitForImgsLoad } from '../../hooks';
-import { SerialisedSpecies } from '../../interfaces/serialised-species';
+import { EvoPath, Pokemon, PokemonDetails, SerialisedSpecies } from '../../interfaces';
+import { useWaitForImgsLoad, useFetchChained } from '../../hooks';
 import { serialiseEvolutionTree, serialisePokemonSpecies } from '../../serialisers';
 import InfoTabsContainer from '../InfoTabsContainer/InfoTabsContainer';
 import PokemonAbout from '../PokemonAbout/PokemonAbout';
 import PokemonMovesTable from '../PokemonMovesTable/PokemonMovesTable';
 import PokemonEvolutionPaths from '../PokemonEvolutionPaths/PokemonEvolutionPaths';
 import PokemonStats from '../PokemonStats/PokemonStats';
+import PokedexScreen from '../PokedexScreen/PokedexScreen';
+import { getPokemonTypecolours } from '../../helpers';
+import PokemonTypePill from '../PokemonTypePill/PokemonTypePill';
+import './PokemonDetailedContainer.scss';
 
 const getAnimatedImgSrc = (id: number, name: string): string | null => {
   // source for pokemon gifs - only has gifs for pokemon up to #0898
@@ -21,106 +24,135 @@ interface Props {
 }
 
 const PokemonDetailedContainer: React.FC<Props> = ({ selectedPokemon }) => {
-  const [pokemonDetailed, setPokemonDetailed] = useState<null | PokemonDetailed>(null);
+  const [pokemonDetails, setPokemonDetails] = useState<null | PokemonDetails>(null);
   const { allImgsLoaded, handleImgLoad, resetImgsLoadCheck } = useWaitForImgsLoad();
-  const speciesFetchHook = useFetch();
-  const evolutionFetchHook = useFetch();
-  const speciesData = speciesFetchHook.data as SerialisedSpecies;
-  const speciesFetchConfig = speciesFetchHook.setFetchConfig;
-  const evolutionPaths = evolutionFetchHook.data as EvoPath[][];
-  const evolutionFetchConfig = evolutionFetchHook.setFetchConfig;
-  const loading = speciesFetchHook.loading || evolutionFetchHook.loading;
+  const { setFetchChain, loading, error, data } = useFetchChained();
 
   useEffect(() => {
     if (!selectedPokemon) return;
-    speciesFetchConfig(
-      `https://pokeapi.co/api/v2/pokemon-species/${selectedPokemon.id}`,
-      serialisePokemonSpecies
-    );
-    setPokemonDetailed(null);
-  }, [selectedPokemon, speciesFetchConfig]);
+    resetImgsLoadCheck(1);
+    setFetchChain([
+      {
+        buildUrl: () => `https://pokeapi.co/api/v2/pokemon-species/${selectedPokemon.id}`,
+        serialiser: serialisePokemonSpecies,
+        buildNextParams: (data: SerialisedSpecies) => ({ evoChainUrl: data.evolutionChain }),
+      },
+      {
+        buildUrl: (params) => params && params.evoChainUrl,
+        serialiser: serialiseEvolutionTree,
+      },
+    ]);
+  }, [selectedPokemon, setFetchChain, resetImgsLoadCheck]);
 
   useEffect(() => {
-    if (speciesData) {
-      evolutionFetchConfig(speciesData.evolutionChain, serialiseEvolutionTree);
-    }
-  }, [speciesData, evolutionFetchConfig]);
+    if (!data) return;
+    const [speciesData, evolutionPaths]: [SerialisedSpecies, EvoPath[][]] = data;
+    const { description, species, genderRatio, eggGroups } = speciesData;
+    const detailed: PokemonDetails = {
+      description,
+      species,
+      genderRatio,
+      eggGroups,
+      evolutionPaths,
+    };
+    setPokemonDetails(detailed);
+  }, [data]);
 
-  useEffect(() => {
-    if (evolutionPaths && selectedPokemon && speciesData) {
-      const { description, species, genderRatio, eggGroups } = speciesData;
-      const { id, name, ...pokemon } = selectedPokemon;
-      const animatedImgSrc = getAnimatedImgSrc(id, name);
-      const detailed: PokemonDetailed = {
-        id,
-        name,
-        description,
-        species,
-        genderRatio,
-        eggGroups,
-        evolutionPaths,
-        animatedImgSrc,
-        ...pokemon,
-      };
-      resetImgsLoadCheck(1);
-      setPokemonDetailed(detailed);
-    }
-  }, [evolutionPaths, selectedPokemon, speciesData, resetImgsLoadCheck]);
+  const typeColours = selectedPokemon && getPokemonTypecolours(selectedPokemon.types[0]);
+  const primaryColour = typeColours ? typeColours.primarycolour : '#8A809D';
+  const secondaryColour = typeColours ? typeColours.primarycolour : '#59bae9';
 
   let content: string | ReactNode = 'Select a Pokémon from the list';
   if (loading) content = 'loading...';
-  else if (pokemonDetailed) {
+  else if (pokemonDetails && selectedPokemon) {
     const infoTabs = [
       {
         label: 'About',
         content: (
           <PokemonAbout
-            species={pokemonDetailed.species}
-            abilities={pokemonDetailed.abilities}
-            genderRatio={pokemonDetailed.genderRatio}
-            eggGroups={pokemonDetailed.eggGroups}
+            species={pokemonDetails.species}
+            abilities={selectedPokemon.abilities}
+            genderRatio={pokemonDetails.genderRatio}
+            eggGroups={pokemonDetails.eggGroups}
           />
         ),
       },
       {
         label: 'Description',
-        content: pokemonDetailed.description,
+        content: pokemonDetails.description,
       },
       {
         label: 'Moves',
         content: (
           <PokemonMovesTable
-            taughtMoves={pokemonDetailed.taughtMoves}
-            levelUpMoves={pokemonDetailed.levelUpMoves}
+            taughtMoves={selectedPokemon.taughtMoves}
+            levelUpMoves={selectedPokemon.levelUpMoves}
+            bgColour={primaryColour}
           />
         ),
       },
       {
         label: 'Evolution Paths',
-        content: <PokemonEvolutionPaths evolutionPaths={pokemonDetailed.evolutionPaths} />,
+        content: (
+          <PokemonEvolutionPaths
+            evolutionPaths={pokemonDetails.evolutionPaths}
+            bgColour={primaryColour}
+          />
+        ),
       },
     ];
 
     content = (
       <Fragment>
-        {!allImgsLoaded && 'loading images'}
-        <div style={{ display: allImgsLoaded ? 'block' : 'none' }}>
-          {pokemonDetailed.name}
-          <img
-            src={pokemonDetailed.animatedImgSrc || pokemonDetailed.imgSrc}
-            alt={pokemonDetailed.name}
-            onLoad={handleImgLoad}
-          />
-          <PokemonStats stats={pokemonDetailed.stats} />
-          {pokemonDetailed.height * 10}cm
-          {pokemonDetailed.weight / 10}kg
+        {!allImgsLoaded && 'loading image'}
+        <div className={allImgsLoaded ? 'pokemon-details' : 'display-none'}>
+          <span className="pokemon-name">{selectedPokemon.name}</span>
+          <div className="pokemon-overview">
+            <img
+              className="pokemon-image"
+              src={
+                getAnimatedImgSrc(selectedPokemon.id, selectedPokemon.name) ||
+                selectedPokemon.imgSrc
+              }
+              alt={selectedPokemon.name}
+              onLoad={handleImgLoad}
+            />
+            <PokemonStats stats={selectedPokemon.stats} barColour={secondaryColour} />
+            <div className="pokemon-extra-info">
+              <div className="pokemon-types">
+                {selectedPokemon.types.map((type) => (
+                  <PokemonTypePill key={type} type={type} />
+                ))}
+              </div>
+              <div className="pokemon-measurements">
+                <span className="measurement-title">Height:</span> {selectedPokemon.height * 10} cm
+                <span className="measurement-title">Weight:</span> {selectedPokemon.weight / 10} kg
+              </div>
+            </div>
+          </div>
           <InfoTabsContainer config={infoTabs} />
         </div>
       </Fragment>
     );
   }
 
-  return <div>{content}</div>;
+  const screenBackground =
+    typeColours && !loading && allImgsLoaded
+      ? `linear-gradient(315deg, ${typeColours.primarycolour} 10%, ${typeColours.secondarycolour} 90%)`
+      : '';
+
+  return (
+    <PokedexScreen
+      styleProps={{
+        height: '100%',
+        padding: '20px',
+        overflow: 'auto',
+        background: screenBackground,
+      }}
+    >
+      {content}
+    </PokedexScreen>
+  );
 };
 
 export default PokemonDetailedContainer;
